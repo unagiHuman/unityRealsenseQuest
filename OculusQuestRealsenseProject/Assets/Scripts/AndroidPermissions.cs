a06e1f6abaf36046a488950b47dcb01e1b397f76
unagiHuman <tigerwoods1206@gmail.com>
2019-08-24 22:21:03 +0900

    [init]

---
+++ OculusQuestRealsenseProject/Assets/Scripts/AndroidPermissions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidPermissions : MonoBehaviour
+{
+#if UNITY_ANDROID && !UNITY_EDITOR
+    void Awake()
+    {
+        if (!UnityEngine.Android.Permission.HasUserAuthorizedPermission(UnityEngine.Android.Permission.Camera))
+        {
+            UnityEngine.Android.Permission.RequestUserPermission(UnityEngine.Android.Permission.Camera);
+
+        }
+
+        using (var javaUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        using (var currentActivity = javaUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+        using (var rsContext = new AndroidJavaClass("com.intel.realsense.librealsense.RsContext"))
+        {
+            Debug.Log(rsContext);
+            rsContext.CallStatic("init", currentActivity);
+        }
+    }
+#endif
+}
